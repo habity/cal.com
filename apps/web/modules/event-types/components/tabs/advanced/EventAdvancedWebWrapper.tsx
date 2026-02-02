700b358157b2e3278c1c183ee23c85c9d1daf7ea
Per Jonsson <per.d.jonsson@gmail.com>
2026-02-02 10:26:30 +0100

    feat(cal): disable field editing for non-admins

--- apps/web/modules/event-types/components/tabs/advanced/EventAdvancedWebWrapper.tsx
+++ apps/web/modules/event-types/components/tabs/advanced/EventAdvancedWebWrapper.tsx
@@ -1,14 +1,21 @@
+import { useSession } from "next-auth/react";
+
 import { localeOptions } from "@calcom/lib/i18n";
+import { UserPermissionRole } from "@calcom/prisma/enums";
 import { trpc } from "@calcom/trpc/react";
 
 import type { EventAdvancedBaseProps } from "./EventAdvancedTab";
 import { EventAdvancedTab } from "./EventAdvancedTab";
 
 const EventAdvancedWebWrapper = ({ ...props }: EventAdvancedBaseProps) => {
-  const connectedCalendarsQuery = trpc.viewer.calendars.connectedCalendars.useQuery();
-  const { data: verifiedEmails } = trpc.viewer.workflows.getVerifiedEmails.useQuery({
-    teamId: props.team?.id,
-  });
+  const session = useSession();
+  const isAdmin = session.data?.user.role === UserPermissionRole.ADMIN;
+  const connectedCalendarsQuery =
+    trpc.viewer.calendars.connectedCalendars.useQuery();
+  const { data: verifiedEmails } =
+    trpc.viewer.workflows.getVerifiedEmails.useQuery({
+      teamId: props.team?.id,
+    });
   return (
     <EventAdvancedTab
       {...props}
@@ -20,6 +27,7 @@ const EventAdvancedWebWrapper = ({ ...props }: EventAdvancedBaseProps) => {
       showBookerLayoutSelector={true}
       verifiedEmails={verifiedEmails}
       localeOptions={localeOptions}
+      isAdmin={isAdmin}
     />
   );
 };
