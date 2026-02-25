0d028092c1efb02075392a2878068b6fc5156fb6
Per Jonsson <per.d.jonsson@gmail.com>
2026-02-25 13:13:28 +0100

    fix(cal): wait for password email

--- apps/web/app/api/auth/forgot-password/route.ts
+++ apps/web/app/api/auth/forgot-password/route.ts
@@ -38,7 +38,13 @@ async function handler(req: NextRequest) {
       select: { name: true, email: true, locale: true },
     });
     // Don't leak info about whether the user exists
-    if (user) passwordResetRequest(user).catch(console.error);
+    if (user) {
+      try {
+        await passwordResetRequest(user);
+      } catch (e) {
+        console.error(e);
+      }
+    }
     return NextResponse.json({ message: "password_reset_email_sent" }, { status: 201 });
   } catch (reason) {
     console.error(reason);
